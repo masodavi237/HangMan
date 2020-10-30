fb364331060f366e40ff7a2feeda3fa85a296021
masodavi237 <masodavi237@2260I-L42-65>
2020-10-30 13:38:16 -0400

    almost finished

--- HangMan/Form1.Designer.cs
+++ HangMan/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HangMan
 {
-    partial class Form1
+    partial class HangMan
     {
         /// <summary>
         /// Required designer variable.
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HangMan));
             this.lblTitle = new System.Windows.Forms.Label();
-            this.pbxBlood = new System.Windows.Forms.PictureBox();
             this.lblWins = new System.Windows.Forms.Label();
             this.lblLosses = new System.Windows.Forms.Label();
             this.lblWinsDisp = new System.Windows.Forms.Label();
@@ -43,6 +44,13 @@
             this.btnOpen = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.lstUsedLetters = new System.Windows.Forms.ListBox();
+            this.imlHangDisplay = new System.Windows.Forms.ImageList(this.components);
+            this.pbxHangDisplay = new System.Windows.Forms.PictureBox();
+            this.pbxBlood = new System.Windows.Forms.PictureBox();
+            this.rdbEasy = new System.Windows.Forms.RadioButton();
+            this.rdbNormal = new System.Windows.Forms.RadioButton();
+            this.rdbHard = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHangDisplay)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxBlood)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,16 +63,6 @@
             this.lblTitle.Size = new System.Drawing.Size(516, 32);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Welcome - Can You Beat the Hang-Man?";
-            // 
-            // pbxBlood
-            // 
-            this.pbxBlood.Image = global::HangMan.Properties.Resources.Blood;
-            this.pbxBlood.Location = new System.Drawing.Point(19, 39);
-            this.pbxBlood.Name = "pbxBlood";
-            this.pbxBlood.Size = new System.Drawing.Size(498, 103);
-            this.pbxBlood.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbxBlood.TabIndex = 1;
-            this.pbxBlood.TabStop = false;
             // 
             // lblWins
             // 
@@ -106,6 +104,7 @@
             // 
             // txtGuess
             // 
+            this.txtGuess.BackColor = System.Drawing.Color.PowderBlue;
             this.txtGuess.Location = new System.Drawing.Point(19, 200);
             this.txtGuess.Name = "txtGuess";
             this.txtGuess.Size = new System.Drawing.Size(100, 20);
@@ -174,19 +173,98 @@
             // 
             // lstUsedLetters
             // 
+            this.lstUsedLetters.BackColor = System.Drawing.Color.PowderBlue;
+            this.lstUsedLetters.Font = new System.Drawing.Font("Castellar", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstUsedLetters.ForeColor = System.Drawing.Color.OrangeRed;
             this.lstUsedLetters.FormattingEnabled = true;
-            this.lstUsedLetters.Location = new System.Drawing.Point(590, 149);
+            this.lstUsedLetters.ItemHeight = 23;
+            this.lstUsedLetters.Location = new System.Drawing.Point(746, 13);
             this.lstUsedLetters.Name = "lstUsedLetters";
-            this.lstUsedLetters.Size = new System.Drawing.Size(165, 199);
+            this.lstUsedLetters.Size = new System.Drawing.Size(42, 395);
             this.lstUsedLetters.TabIndex = 14;
             // 
-            // Form1
+            // imlHangDisplay
+            // 
+            this.imlHangDisplay.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imlHangDisplay.ImageStream")));
+            this.imlHangDisplay.TransparentColor = System.Drawing.Color.Transparent;
+            this.imlHangDisplay.Images.SetKeyName(0, "0.jpg");
+            this.imlHangDisplay.Images.SetKeyName(1, "1.jpg");
+            this.imlHangDisplay.Images.SetKeyName(2, "2.jpg");
+            this.imlHangDisplay.Images.SetKeyName(3, "3.jpg");
+            this.imlHangDisplay.Images.SetKeyName(4, "4.jpg");
+            this.imlHangDisplay.Images.SetKeyName(5, "5.jpg");
+            this.imlHangDisplay.Images.SetKeyName(6, "6.jpg");
+            this.imlHangDisplay.Images.SetKeyName(7, "7.jpg");
+            this.imlHangDisplay.Images.SetKeyName(8, "8.jpg");
+            this.imlHangDisplay.Images.SetKeyName(9, "9.jpg");
+            this.imlHangDisplay.Images.SetKeyName(10, "10.jpg");
+            // 
+            // pbxHangDisplay
+            // 
+            this.pbxHangDisplay.Location = new System.Drawing.Point(540, 105);
+            this.pbxHangDisplay.Name = "pbxHangDisplay";
+            this.pbxHangDisplay.Size = new System.Drawing.Size(200, 297);
+            this.pbxHangDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxHangDisplay.TabIndex = 15;
+            this.pbxHangDisplay.TabStop = false;
+            // 
+            // pbxBlood
+            // 
+            this.pbxBlood.Image = global::HangMan.Properties.Resources.Blood_removebg_preview;
+            this.pbxBlood.Location = new System.Drawing.Point(19, 39);
+            this.pbxBlood.Name = "pbxBlood";
+            this.pbxBlood.Size = new System.Drawing.Size(498, 103);
+            this.pbxBlood.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxBlood.TabIndex = 1;
+            this.pbxBlood.TabStop = false;
+            // 
+            // rdbEasy
+            // 
+            this.rdbEasy.AutoSize = true;
+            this.rdbEasy.Location = new System.Drawing.Point(411, 200);
+            this.rdbEasy.Name = "rdbEasy";
+            this.rdbEasy.Size = new System.Drawing.Size(48, 17);
+            this.rdbEasy.TabIndex = 16;
+            this.rdbEasy.TabStop = true;
+            this.rdbEasy.Text = "Easy";
+            this.rdbEasy.UseVisualStyleBackColor = true;
+            this.rdbEasy.CheckedChanged += new System.EventHandler(this.rdbEasy_CheckedChanged);
+            // 
+            // rdbNormal
+            // 
+            this.rdbNormal.AutoSize = true;
+            this.rdbNormal.Location = new System.Drawing.Point(411, 223);
+            this.rdbNormal.Name = "rdbNormal";
+            this.rdbNormal.Size = new System.Drawing.Size(58, 17);
+            this.rdbNormal.TabIndex = 17;
+            this.rdbNormal.TabStop = true;
+            this.rdbNormal.Text = "Normal";
+            this.rdbNormal.UseVisualStyleBackColor = true;
+            this.rdbNormal.CheckedChanged += new System.EventHandler(this.rdbNormal_CheckedChanged);
+            // 
+            // rdbHard
+            // 
+            this.rdbHard.AutoSize = true;
+            this.rdbHard.Location = new System.Drawing.Point(411, 246);
+            this.rdbHard.Name = "rdbHard";
+            this.rdbHard.Size = new System.Drawing.Size(48, 17);
+            this.rdbHard.TabIndex = 18;
+            this.rdbHard.TabStop = true;
+            this.rdbHard.Text = "Hard";
+            this.rdbHard.UseVisualStyleBackColor = true;
+            this.rdbHard.CheckedChanged += new System.EventHandler(this.rdbHard_CheckedChanged);
+            // 
+            // HangMan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackColor = System.Drawing.Color.Wheat;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rdbHard);
+            this.Controls.Add(this.rdbNormal);
+            this.Controls.Add(this.rdbEasy);
+            this.Controls.Add(this.pbxHangDisplay);
             this.Controls.Add(this.lstUsedLetters);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnOpen);
@@ -201,8 +279,10 @@
             this.Controls.Add(this.lblWins);
             this.Controls.Add(this.pbxBlood);
             this.Controls.Add(this.lblTitle);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "HangMan";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHangDisplay)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxBlood)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -226,6 +306,11 @@
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.ListBox lstUsedLetters;
+        private System.Windows.Forms.PictureBox pbxHangDisplay;
+        private System.Windows.Forms.ImageList imlHangDisplay;
+        private System.Windows.Forms.RadioButton rdbEasy;
+        private System.Windows.Forms.RadioButton rdbNormal;
+        private System.Windows.Forms.RadioButton rdbHard;
     }
 }
 
--- HangMan/Form1.cs
+++ HangMan/Form1.cs
@@ -11,59 +11,129 @@ using System.Windows.Forms;
 
 namespace HangMan
 {
-    public partial class Form1 : Form
+    public partial class HangMan : Form
     {
         Random Generator = new Random();
         List<string> words = new List<string>();
         string hiddenWord;
         string guess;
+        string lostgame;
+        string wingame;
+        int wins;
+        int losses;
         int incorrect;
         int randomForIndex;
         int indexLength;
-        int indexOfLetter;
+        int indexOfList;
         bool contains;
         bool sameLetter;
+        bool wordChosen;
+        bool diffSet;
+        bool easy;
+        bool normal;
+        bool hard;
         char check;
+        List<Image> hangmans;
 
 
 
          
         
 
-        public Form1()
+        public HangMan()
         {
             InitializeComponent();
+            
         }
 
         private void btmGuess_Click(object sender, EventArgs e)
         {
-            guess = (txtGuess.Text).ToUpper();
-            contains = hiddenWord.Contains(guess);
-            
-            if (guess.Length == 1)
+            if (wordChosen && diffSet)
             {
-                if (contains)
-                {
-                    lblError.Text = "Does contain";
-                    for (int i = 0; i < hiddenWord.Length; i++)
-                    {
-                        check = hiddenWord[i];
-                        sameLetter = guess.Contains(check);
-                        if (sameLetter)
-                        {
-                            
-                            indexOfLetter = i;
-                            hiddenWord = hiddenWord.Remove(i, 1);
-                            hiddenWord = hiddenWord.Insert(i - 1, "0");
-                            lblWord.Text = lblWord.Text.Remove((i * 2) + 1, 1);
-                            lblWord.Text =  lblWord.Text.Insert((i * 2) - 1, guess);
-                            lblError.Text = hiddenWord;
+                guess = (txtGuess.Text).ToUpper();
+                contains = hiddenWord.Contains(guess);
+                indexOfList = lstUsedLetters.FindString(guess);
+                
 
+                if (guess.Length == 1)
+                {
+                    if (indexOfList == -1)
+                    {
+                        if (contains)
+                        {
+                            for (int i = 0; i < hiddenWord.Length; i++)
+                            {
+                                check = hiddenWord[i];
+                                sameLetter = guess.Contains(check);
+                                if (sameLetter)
+                                {
+
+                                    
+                                    if (indexOfList == -1)
+                                    {
+                                        
+                                        lstUsedLetters.Items.Add(guess);
+                                        indexOfList = lstUsedLetters.FindString(guess);
+                                        
+                                    }
+                                    hiddenWord = hiddenWord.Remove(i, 1);
+                                    hiddenWord = hiddenWord.Insert(i, "0");
+                                    lblWord.Text = lblWord.Text.Remove((i * 2), 1);
+                                    lblWord.Text = lblWord.Text.Insert((i * 2), guess);
+                                    lblError.Text = incorrect + "";
+
+                                }
+                            }
+                        }
+
+                        else
+                        {
+                            incorrect = incorrect + 1;
+                            lstUsedLetters.Items.Add(guess);
+                            pbxHangDisplay.Image = hangmans[incorrect];
+
+                            if (incorrect >= 10)
+                            {
+                                losses += 1;
+                                lblLossesDisp.Text = losses + "";
+                                wordChosen = false;
+
+                                for (int i = 0; i <= hiddenWord.Length-1; i++)
+                                {
+
+                                    System.Threading.Thread.Sleep(50);
+                                    if (hiddenWord.Length <= lostgame.Length)
+                                    {
+                                        if (i <= hiddenWord.Length)
+                                        {
+                                            lblWord.Text = lblWord.Text.Remove((i * 2), 1);
+                                            lblWord.Text = lblWord.Text.Insert((i * 2), (lostgame[i] + ""));
+                                            
+                                        }
+                                        else
+                                        {
+                                            /*lblWord.Text = lblWord.Text.Insert((i * 2), (lostgame[i] + ""));
+                                            System.Threading.Thread.Sleep(50);*/
+                                        }
+
+                                    }
+                                    else
+                                    {
+
+                                    }
+                                }
+
+
+                            }
                         }
                     }
-
+                    
                 }
-
+                
+            }
+            else
+            {
+                lblError.Text = "Please click 'New Word' and set difficulty";
             }
 
 
@@ -90,11 +160,30 @@ namespace HangMan
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            
+
             indexLength = words.Count;
-            randomForIndex = Generator.Next(0, indexLength + 1);
+            randomForIndex = Generator.Next(0, indexLength);
+            lstUsedLetters.Items.Clear();
+
+            if (easy)
+            {
+                incorrect = 0;
+                pbxHangDisplay.Image = hangmans[incorrect];
+            }
+            else if (normal)
+            {
+                incorrect = 2;
+                pbxHangDisplay.Image = hangmans[incorrect];
+            }
+            else if (hard)
+            {
+                incorrect = 8;
+                pbxHangDisplay.Image = hangmans[incorrect];
+            }
+           
+
             
-            if (indexLength != 0)
+            if (indexLength != 0 && diffSet)
             {
                 hiddenWord = words[randomForIndex];
                 lblWord.Text = "";
@@ -105,8 +194,13 @@ namespace HangMan
                     lblError.Text = hiddenWord;
                 }
 
+                wordChosen = true;
                 
                 
+            }
+            else if (!diffSet)
+            {
+                lblError.Text = "Please select a difficulty";
             }
             
             else
@@ -118,6 +212,52 @@ namespace HangMan
             
 
             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hangmans = new List<Image>();
+            hangmans.Add(global::HangMan.Properties.Resources._0);
+            hangmans.Add(global::HangMan.Properties.Resources._1);
+            hangmans.Add(global::HangMan.Properties.Resources._2);
+            hangmans.Add(global::HangMan.Properties.Resources._3);
+            hangmans.Add(global::HangMan.Properties.Resources._4);
+            hangmans.Add(global::HangMan.Properties.Resources._5);
+            hangmans.Add(global::HangMan.Properties.Resources._6);
+            hangmans.Add(global::HangMan.Properties.Resources._7);
+            hangmans.Add(global::HangMan.Properties.Resources._8);
+            hangmans.Add(global::HangMan.Properties.Resources._9);
+            hangmans.Add(global::HangMan.Properties.Resources._10);
+
+            incorrect = 0;
+            pbxHangDisplay.Image = global::HangMan.Properties.Resources._0;
+
+            lostgame = "YOU LOSE";
+            wingame = "YOU WIN";
+        }
+
+        private void rdbEasy_CheckedChanged(object sender, EventArgs e)
+        {
+            diffSet = true;
+            easy = true;
+            normal = false;
+            hard = false;
+        }
+
+        private void rdbNormal_CheckedChanged(object sender, EventArgs e)
+        {
+            diffSet = true;
+            normal = true;
+            easy = false;
+            hard = false;
+        }
+
+        private void rdbHard_CheckedChanged(object sender, EventArgs e)
+        {
+            diffSet = true;
+            hard = true;
+            easy = false;
+            normal = false;
         }
     }
 }
